a338825706094131157a85c41f49bd715663d2c4
Matt McCormick <matt.mccormick@kitware.com>
2014-05-20 20:37:29 -0400

    ENH: Reorganize as ITK module.

---
+++ itk-module.cmake
@@ -0,0 +1,10 @@
+set(DOCUMENTATION "This module contains classes for performing phase symmetry
+filtration.")
+
+itk_module(PhaseSymmetry
+  DEPENDS
+  TEST_DEPENDS
+    ITKTestKernel
+  DESCRIPTION
+    "${DOCUMENTATION}"
+  )
